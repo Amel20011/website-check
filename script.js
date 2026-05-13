dcde65135f916b61ceba633d429de4664cdbbfa8
Amel20011 <amelcns8@gmail.com>
2026-05-13 18:57:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function cekStatus(){
+async function cekStatus(){
 
   const invoice = document
     .getElementById("invoice")
@@ -8,15 +8,64 @@ function cekStatus(){
   const hasil = document
     .getElementById("hasil");
 
+  const loadingBox = document
+    .getElementById("loadingBox");
+
+  hasil.innerHTML = "";
+  loadingBox.innerHTML = "";
+
+  const steps = [
+    "🔍 Mengecek kode invoice...",
+    "🧾 Mengecek ID customer...",
+    "📦 Mengecek proses pengerjaan...",
+    "👥 Mengecek tim produksi..."
+  ];
+
+  for(let i = 0; i < steps.length; i++){
+
+    loadingBox.innerHTML += `
+      <div class="loading-step">
+        ${steps[i]}
+      </div>
+    `;
+
+    await new Promise(resolve =>
+      setTimeout(resolve, 1000)
+    );
+
+  }
+
+  loadingBox.innerHTML = "";
+
   if(database[invoice]){
 
     hasil.innerHTML = `
       <div class="result-box">
-        <h3>${database[invoice].nama}</h3>
+
+        <h3>
+          ✅ Invoice ditemukan
+        </h3>
+
+        <br>
+
+        <p>
+          Customer:
+          ${database[invoice].nama}
+        </p>
+
+        <br>
 
         <p class="success">
-          Status: ${database[invoice].status}
+          Status:
+          ${database[invoice].status}
         </p>
+
+        <br>
+
+        <small>
+          Status telah masuk ke database kami
+        </small>
+
       </div>
     `;
 
@@ -24,9 +73,12 @@ function cekStatus(){
 
     hasil.innerHTML = `
       <div class="result-box">
+
         <p class="error">
-          Invoice Tidak Ditemukan
+          ⚠️ Invoice belum masuk ke database,
+          mungkin membutuhkan beberapa saat.
         </p>
+
       </div>
     `;
 

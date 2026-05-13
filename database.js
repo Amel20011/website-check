a1bceec7d772b2926a34bc8e9daf5d34d3b93463
Amel20011 <amelcns8@gmail.com>
2026-05-13 18:49:04 +0800

    Create database.js

---
+++ database.js
@@ -0,0 +1,23 @@
+const database = {
+
+  "#BAND001": {
+    nama: "Customer Basic",
+    status: "Dalam Proses"
+  },
+
+  "#BAND002": {
+    nama: "Customer Premium",
+    status: "Sedang Mixing"
+  },
+
+  "#BAND003": {
+    nama: "Customer Exclusive",
+    status: "Dalam Pemeriksaan"
+  },
+
+  "#SPECIAL999": {
+    nama: "VIP Customer",
+    status: "Selesai & Dikirim"
+  }
+
+};
